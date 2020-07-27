dc945916e141a9f98e6e563a13371f9f6b48ae90
adam-spice <NoRton12#4>
2020-07-27 18:34:38 +1200

    initial commit

---
+++ Player/Player.gd
@@ -0,0 +1,56 @@
+extends KinematicBody2D
+
+var motion = Vector2.ZERO
+
+const SPEED = 1500
+const GRAVITY = 300
+const UP = Vector2(0,-1)
+const JUMP_SPEED = 5000
+
+func _physics_process(delta):
+	apply_gravity()
+	jump()
+	move()
+	animate()
+	move_and_slide(motion, UP)
+	
+func apply_gravity():
+	if is_on_floor():
+		motion.y = 0
+	else:
+		motion.y += GRAVITY
+		
+func jump():
+	if Input.is_action_pressed("jump") and is_on_floor():
+		motion.y -= JUMP_SPEED
+		
+func move():
+	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
+		motion.x = -SPEED
+	elif Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
+		motion.x = SPEED
+	else:
+		motion.x = 0
+		
+func animate():
+	if motion.y < 0:
+		$AnimatedSprite.play("jump")
+	elif motion.x != 0:
+		if motion.x < 0:
+			$AnimatedSprite.flip_h = true
+		else: 
+			$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play("walk")
+		
+	else:
+		$AnimatedSprite.play("idle")
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
